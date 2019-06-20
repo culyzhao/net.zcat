6103d3a3531c88301bb3e5283496665f12f15451
culyzhao <culyzhao@gmail.com>
2019-06-20 09:14:41 +0800

    Update zcat.init.sh

--- script/zcat.init.sh
+++ script/zcat.init.sh
@@ -3,3 +3,8 @@ curl -L https://setup.ius.io/ | bash
 yum groupinstall 'Development Tools' -y
 yum install net-tools wget htop zip unzip vim pcre pcre-devel zlib zlib-devel openssl openssl-devel -y
 
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
